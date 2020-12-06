5562c7a916247bcff439580723d35d056d2198bd
Chris Mears <chris.mears@gmail.com>
2020-12-05 23:01:09 -0800

    feat: :sparkles: Add !info command
    Brewery lookup via Open Brewery DB API

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
+const axios = require("axios");
 const { Client, MessageEmbed } = require("discord.js");
 const client = new Client();
+const { prefix } = require("./config.json");
 
 const REPLY_MESSAGES = ["hi there! 🍻", "how's it going? 🍻", "cheers! 🍻"];
 
@@ -14,11 +16,9 @@ function readyDiscord() {
 }
 
 function receivedMessage(message) {
-  // Prevents spamming
-  // Ignore messages from bots
-  if (message.author.bot) return false;
-  // Ignore broadcast messages
+  // Ignore messages from bots & broadcast messages
   if (
+    message.author.bot ||
     message.content.includes("@here") ||
     message.content.includes("@everyone")
   )
@@ -38,4 +38,92 @@ function receivedMessage(message) {
       message.reply(replyMessage);
     }
   }
+
+  // Respond to commands
+  if (message.content.startsWith(prefix)) {
+    const args = message.content.slice(prefix.length).trim().split(" ");
+    const command = args.shift().toLowerCase();
+
+    // info - Brewery Search with Open Brewery DB
+    if (command === "info") {
+      if (!args.length) {
+        // Ensure there's a search argument
+        return message.channel.send(
+          `Sorry, I need to search for something. Try something like, '!info modern times'`
+        );
+      } else {
+        brewery_name = args.join(" ");
+        brewery_query = encodeURIComponent(brewery_name);
+
+        console.log(`🔍 Searching OBDB for '${brewery_query}'...`);
+
+        // Promise request to OBDB
+        return axios
+          .get(
+            `https://api.openbrewerydb.org/breweries?by_name=${brewery_query}`
+          )
+          .then((res) => {
+            breweries = res.data;
+            console.log(`⮑ Found '${breweries.length}' breweries.`);
+
+            if (breweries.length == 0) {
+              // No breweries returned
+              message.channel.send(
+                `I didn't find any breweries matching '${brewery_name}'. 😓`
+              );
+            } else if (breweries.length == 1) {
+              // Only 1 brewery returned
+              brewery = breweries[0];
+              message.channel.send(
+                `I found ${brewery.name} in ${brewery.city}, ${brewery.state}!`
+              );
+
+              if (brewery.website_url) {
+                const breweryEmbed = new MessageEmbed()
+                  .setColor("#FBBF24")
+                  .setTitle(brewery.name)
+                  .setURL(brewery.website_url)
+                  .setDescription(
+                    `${brewery.name} - ${brewery.street}, ${brewery.city}, ${brewery.state} ${brewery.postal_code}`
+                  );
+
+                message.channel.send(breweryEmbed);
+              }
+            } else {
+              // More than 1 brewery returned
+              if (breweries.length > 5) {
+                $suffixMessage = "Here are the first 5:";
+              } else {
+                $suffixMessage = "Here they are:";
+              }
+              message.channel.send(
+                `I found ${breweries.length} breweries matching '${brewery_name}'. ${$suffixMessage}`
+              );
+              embedBreweryFields(message, breweries.slice(0, 5));
+            }
+          })
+          .catch((err) => {
+            // Fail silently and log
+            console.log(err);
+          });
+      }
+    }
+  }
+}
+
+function embedBreweryFields(message, breweries) {
+  let fields = [];
+
+  for (let i = 0; i < breweries.length; i++) {
+    const b = breweries[i];
+    const field = {
+      name: b.name,
+      value: `${b.city}, ${b.state}`,
+    };
+    fields.push(field);
+  }
+
+  const breweryEmbed = new MessageEmbed().setColor("#FBBF24").addFields(fields);
+
+  message.channel.send(breweryEmbed);
 }
